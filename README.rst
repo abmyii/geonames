1428ce846464657d046bdaaab43f0b3a1c71ed4e
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2019-12-17 21:07:01 +0000

    Add PyPI files

---
+++ README.rst
@@ -0,0 +1,13 @@
+geonames
+========
+
+Library to query places from GeoName dumps (http://download.geonames.org/export/dump/).
+
+Installation
+------------
+
+geonames requires Python 2.7 or 3.5+ as well as ``numpy`` and ``pandas`` packages. It can be installed with,
+
+.. code::
+
+    pip install geonames
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Topic :: Software Development
+Operating System :: POSIX
+Operating System :: Unix
+"""
+
+
+setuptools.setup(
+    name="geonames-lib",
+    version="0.1",
+    author="abmyii",
+    author_email="abdurrahmaaniqbal@hotmail.com",
+    description="Library for working with GeoNames dump",
+    long_description=long_description,
+    url="https://github.com/abmyii/geonames",
+    packages=setuptools.find_packages(),
+    py_modules=['geonames'],
+    install_requires=['fuzzywuzzy', 'pandas'],
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+)
